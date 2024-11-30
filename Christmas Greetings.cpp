c529253aa6ed684fef724cf8a3e6bba0fc0a2fab
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-11-30 13:55:42 +0530

    CHRISTGREET

---
+++ Christmas Greetings.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X;
+	cin>>X;
+	if(X==25){
+	    cout<<"CHRISTMAS"<<endl;
+	}
+	else {
+	    cout<<"ORDINARY"<<endl;
+	}
+
+}
