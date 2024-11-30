a816514d653a024152182f56e73eed115cb74398
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-11-30 13:53:38 +0530

    START01

---
+++ Number Mirror.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    // Do not print anything before taking input
+    cin >> N;
+    cout<<N;
+    
+    // Output the value of a in the line below
+    
+}
