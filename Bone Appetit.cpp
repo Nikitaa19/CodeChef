cf369624337385f3364cac3640eb7dda78ddbb4a
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-01 20:29:42 +0530

    BNE_APT

---
+++ Bone Appetit.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,m,x,y;
+	cin>>n>>m;
+	cout<<endl;
+	cin>>x>>y;
+	int bones=n*x;
+	int blood=m*y;
+	int final=bones+blood;
+	cout<<final<<endl;
+
+}
