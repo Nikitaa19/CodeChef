acd48f86b35650260526f3d16daeaf37471563bf
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-01 20:21:25 +0530

    IED

---
+++ International Education Day!.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c;
+	cin>>a>>b>>c;
+	if((a*c)>(b*c)){
+	    cout<<a*c<<endl;
+	}
+	else if((b*c)>(a*c)){
+	    cout<<b*c<<endl;
+	}
+
+}
