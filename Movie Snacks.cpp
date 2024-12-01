4ab590aece7564f05d15b45d71efe6986a6b32d1
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-01 20:10:34 +0530

    MOVPR

---
+++ Movie Snacks.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y,z,ci,cc,mc;
+	cin>>x>>y>>z;
+	ci=2*x+3*y;
+	cc=2*z+y;
+	mc=min(ci,cc);
+	cout<<mc;
+
+}
