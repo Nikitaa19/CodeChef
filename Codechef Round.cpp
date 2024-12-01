7f9997fc3319b18e5b9d720b5d702ceff2adb140
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-01 19:46:11 +0530

    CODECHEF

---
+++ Codechef Round.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N;
+	cin>>N;
+	if(N==4){
+	    cout<<"YES"<<endl;
+	}
+	else {
+	    cout<<"NO"<<endl;
+	}
+
+}
