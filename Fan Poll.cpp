b8868a554429185a0ed9d1673b130842b73e8f46
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-01 20:23:28 +0530

    FIZZBUZZ2301

---
+++ Fan Poll.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c;
+	cin>>a>>b>>c;
+	a!=b;
+	a!=c;
+	b!=c;
+	if(a>b && a>c){
+	    cout<<"Yes"<<endl;
+	}
+	else{
+	    cout<<"No"<<endl;
+	}
+
+}
