a4c7ca05cfe75cc81ea4ab1a8d3334ad091f9302
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-01 19:53:17 +0530

    MANGOLASSI

---
+++ Summer Time.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X;
+	cin>>X;
+	if(X>35){
+	    cout<<"YES"<<endl;
+	}
+	else {
+	    cout<<"NO"<<endl;
+	}
+
+}
