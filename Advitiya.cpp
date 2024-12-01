24de3ffe65c5adf4900a5f4fe965b0bf9bc41f4d
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-01 19:54:01 +0530

    ADVITIYA1

---
+++ Advitiya.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N;
+	cin>>N;
+	if(N==16 || N==17 || N==18){
+	    cout<<"ADVITIYA"<<endl;
+	}
+	else {
+	    cout<<"WAITING FOR ADVITIYA";
+	}
+
+}
