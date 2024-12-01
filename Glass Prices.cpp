f30cc0c43590120d734b6dde6789a686d5db9590
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-01 19:40:36 +0530

    GLPR

---
+++ Glass Prices.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	if(y<=2*x){
+	    cout<<"METAL"<<endl;
+	}
+	else{
+	    cout<<"PLASTIC"<<endl;
+	}
+
+}
