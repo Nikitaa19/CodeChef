b7422ebf5ad016c98713e1fba36d05789fe0d555
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-01 20:28:33 +0530

    PUZHUNT

---
+++ Puzzle Hunt.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n>=6 && n<=8){
+	    cout<<"Yes"<<endl;
+	}
+	else{
+	    cout<<"No"<<endl;
+	}
+
+}
