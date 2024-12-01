487dfa3e42d9ee8e8316229401132a02e3b53d67
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-01 20:07:08 +0530

    FIT

---
+++ Fitness.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        // your code goes here
+        int total=5*(2*x);
+        cout<<total<<endl;
+        
+    }
+}
