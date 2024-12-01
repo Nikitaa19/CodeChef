903e953d2ecb326d29c78d8e54f4ed9b51809729
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-01 19:57:16 +0530

    HEALSE

---
+++ Healthy Sleep.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int h;
+	cin>>h;
+	if(h<8){
+	    cout<<"LESS"<<endl;
+	}
+	else if(h==8){
+	    cout<<"PERFECT"<<endl;
+	}
+	else{
+	    cout<<"MORE"<<endl;
+	}
+
+}
