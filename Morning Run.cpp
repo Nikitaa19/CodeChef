154044072de164275f85c59a9860b6b321914f27
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-05 23:58:01 +0530

    MORNINGRUN

---
+++ Morning Run.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	if(2*(x+y)>=1000){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+
+}
