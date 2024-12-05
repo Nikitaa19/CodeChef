72ab44500da41158c329d9148554c4cb70268b0f
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-06 00:01:06 +0530

    WAITTIME

---
+++ Waiting Time.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int k,x;
+	    cin>>k>>x;
+	    k=k*7;
+	    int days=k-x;
+	    cout<<days<<endl;
+	}
+
+}
