95a8992f45ae7cbe85ddc703dcad7e31c504000d
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-05 23:32:13 +0530

    BESTOFTWO

---
+++ Best of Two.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>y){
+	        cout<<x<<endl;
+	    }
+	    else if(x<y){
+	        cout<<y<<endl;
+	    }
+	    else{
+	        cout<<x<<endl;
+	    }
+	}
+
+}
