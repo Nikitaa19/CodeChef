a47b7fefbd9016298e9b1e53e31f65ae0ace2845
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-05 23:31:26 +0530

    KINGTOR

---
+++ Tour of King.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m;
+	    cin>>n>>m;
+	    int five=n*5;
+	    int seven=m*7;
+	    int total=five+seven;
+	    cout<<total<<endl;
+	}
+
+}
