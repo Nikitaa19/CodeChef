35bc7c65fc34bd47879404bed99a02c7d5e3adb1
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-05 23:31:48 +0530

    OLYMPICS24

---
+++ Olympics 2024.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int g,s,b,rem=15;
+	cin>>g>>s>>b;
+	if(g<=5 || s<=5 || b<=5){
+	    cout<<rem-(g+s+b)<<endl;
+	}
+
+}
