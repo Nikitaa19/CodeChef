6851a25baa9ac6dab2f79d15efe08ace4644190f
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-09 00:12:54 +0530

    CHESSTIME

---
+++ Chess Time.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int x=n*6;
+	    int hrs=x/2;
+	    cout<<hrs<<endl;
+	}
+
+}
