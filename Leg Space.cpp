6748881413585b0983aba1fd97948b9bc4125b33
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-09 00:09:44 +0530

    LEGSP

---
+++ Leg Space.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,m;
+	cin>>n>>m;
+	if(m>n){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+
+}
