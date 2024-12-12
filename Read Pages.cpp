8d1bc594af1c90bc250a157f6e21a4e157c60ac3
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-12 20:21:13 +0530

    READPAGES

---
+++ Read Pages.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x,y;
+	    cin>>n>>x>>y;
+	    if(x*y<n){
+	        cout<<"NO"<<endl;
+	    }
+	    else{
+	        cout<<"YES"<<endl;
+	    }
+	}
+
+}
