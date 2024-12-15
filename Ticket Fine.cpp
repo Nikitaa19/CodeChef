329a7ee21d8be5d6d82a92dd90552d9115364ce5
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-15 21:11:14 +0530

    TCKTFINE

---
+++ Ticket Fine.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,p,q;
+	    cin>>x>>p>>q;
+	    int fine=x*(p-q);
+	    cout<<fine<<endl;
+	}
+
+}
