9b13cb24729a7584209ff2fe5df74d0e1342c83d
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-15 21:11:51 +0530

    ONEFULPAIRS

---
+++ Oneful Pairs.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    if(a+b+(a*b)==111){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+	// your code goes here
+	return 0;
+}
