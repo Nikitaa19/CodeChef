04a264f7aa8856b3e2e0f9bb48f8a89e88bfd0e1
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-16 21:01:32 +0530

    VIDEOWORTH

---
+++ Worth of a Video.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int s;
+	    cin>>s;
+	    int words=s*24*1000;
+	    cout<<words<<endl;
+	}
+
+}
