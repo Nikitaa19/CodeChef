05ee2fd0157d33ea1d9e7dfd5fac13e57c400f99
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-16 21:01:15 +0530

    SIXFRIENDS

---
+++ Six Friends.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int a=x*3;
+	    int b=y*2;
+	    int z=min(a,b);
+	    cout<<z<<endl;
+	}
+}
