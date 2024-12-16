1108fd400bf98ef45e5e16d12d443cd365828754
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-16 21:01:52 +0530

    CWIREFRAME

---
+++ Chef and Wire Frames.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m,x;
+	    cin>>n>>m>>x;
+	    int cost=((2*n)+(2*m))*x;
+	    cout<<cost<<endl;
+	}
+
+}
