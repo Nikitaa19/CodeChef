044462a46573ffd57a2a7dddb3a72bddae37cafb
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2024-12-31 23:25:50 +0530

    DISCNT

---
+++ Discount.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    int discount=100-x;
+	    cout<<discount<<endl;
+	}
+
+}
