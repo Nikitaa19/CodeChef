97f9c3728320dfbb4b3c8635e57d5591188adeb2
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2025-01-01 20:22:38 +0530

    PARLIAMENT

---
+++ Parliament.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    float n;
+	    int x;
+	    cin>>n>>x;
+	    float y=n/2;
+	    if(y<=x){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
