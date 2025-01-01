a5ffc72ced925ec45a8bf3039a562542e8cea003
Nikita Agarwal <112479074+Nikitaa19@users.noreply.github.com>
2025-01-01 20:23:59 +0530

    PASSTHEEXAM

---
+++ Pass the Exam.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    if(a>=10 && b>=10 && c>=10 && a+b+c>=100){
+	        cout<<"PASS"<<endl;
+	    }
+	    else{
+	        cout<<"FAIL"<<endl;
+	    }
+	}
+
+}
